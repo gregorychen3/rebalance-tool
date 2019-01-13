e88ce22c87a46be2ce34790f2ef8798169f0c398
Gregory Chen <gregorychen3@gmail.com>
2019-01-13 14:46:08 -0500

    target alloc to show weights adding up to 1 instead of percentages

--- internal/portfolio/asset_alloc.go
+++ internal/portfolio/asset_alloc.go
@@ -16,16 +16,18 @@ func NewAssetAlloc(dom float64, intl float64, bond float64) *AssetAlloc {
 	}
 }
 
-type AssetAllocation map[string]int
+type AssetAllocation map[string]float64
 
 func NewAssetAllocation(weights map[string]int) (AssetAllocation, error) {
+	ret := AssetAllocation{}
 	sum := 0
-	for _, v := range weights {
+	for k, v := range weights {
+		ret[k] = float64(v) / 100.0
 		sum += v
 	}
 	if sum != 100 {
 		return nil, errors.New("weights do not add up to 100%")
 	}
 
-	return weights, nil
+	return ret, nil
 }
